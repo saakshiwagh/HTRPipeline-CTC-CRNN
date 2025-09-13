462e8275b3e17623972ce84f74c84c1b67a0a111
saakshiwagh <studioussaakshi0123@gmail.com>
2025-09-13 22:00:30 +0530

    first commit

---
+++ extra_folder/test_htr.py
@@ -0,0 +1,8 @@
+import cv2
+from htr_pipeline import read_page, DetectorConfig, LineClusteringConfig, ReaderConfig
+
+print("HTR Pipeline imported successfully!")
+print("Testing basic functionality...")
+
+# You can now test with a sample image
+# Make sure to update the model paths to where you place the downloaded models
